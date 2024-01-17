db412da9f1de8f20776e6c57bbed4dd89b2b19b8
Etienne Hurand <informatique@etiennehurand.fr>
2024-01-17 07:56:32 +0100

    App.xaml Loads MainMenu.xaml

---
+++ App/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xaml;
+using System.Xml;
+
+namespace BookshopWpf
+{
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+        }
+    }
+
+}
--- Stock/StockWindow.xaml.cs
+++ Stock/StockWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,17 +8,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MahApps.Metro;
-using MahApps.Metro.Controls;
 
 namespace BookshopWpf
 {
     /// <summary>
-    /// Interaction logic for MainMenu.xaml
+    /// Interaction logic for StockWindow.xaml
     /// </summary>
-    public partial class MainMenu : MetroWindow
+    public partial class StockWindow : Window
     {
-        public MainMenu()
+        public StockWindow()
         {
             InitializeComponent();
         }
